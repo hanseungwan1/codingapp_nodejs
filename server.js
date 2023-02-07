f310944bae1f6ed64484213787b9009d12fc6869
hanseungwan1 <seungwanhan542@gmail.com>
2023-02-07 11:47:21 +0900

    post기능추가, 부트스트랩으로 페이지 꾸미기

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true})) 
 
 app.listen(8080, function() {
     console.log('listening on 8080')
@@ -10,3 +11,10 @@ app.get('/pet',function(req,res){
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html')
 })
+app.get('/write',function(req,res){
+    res.sendFile(__dirname+'/write.html')
+})
+app.post('/add', function(req,res){
+    res.send('전송완료')
+    console.log(req.body);
+});
